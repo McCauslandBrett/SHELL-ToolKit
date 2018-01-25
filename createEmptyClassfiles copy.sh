98178e4fca86b359afa0ec07e57e6c69253f60d7
brettmccausland <brettmccausland@yahoo.com>
2018-01-25 14:47:20 -0800

    class files and main files

---
+++ createEmptyClassfiles copy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+touch $1.hh
+echo "#ifndef $1_hh
+#define $1_hh
+
+class $1
+{
+  public:
+    $1();
+    ~$1();
+
+  private:
+};
+#endif" >> $1.hh
+
+
+touch $1.cc
+echo "#include \"./$1.hh \"
+
+$1::$1()
+{}
+
+$1::~$1()
+{}" >> $1.cc
